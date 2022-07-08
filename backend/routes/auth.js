33649747405f5c4a143a3e15869476c843224399
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-08 11:42:10 +0530

    Express-Validator+unique_field_validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 //Create a  user using : POST "/api/auth/", doesn't requires Authentication
 
-router.post("/", (req, res) => {
-  console.log(req.body); //printing request body inside console after editing json content in thunderclient
-  // res.send("Hello Sabyasachi"); //gives response "Hello Sabyasachi"
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body(
+      "password",
+      "Enter a valid password [Password must be atleast 5 characters]"
+    ).isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "Please enter a unique value for email",
+          message: err.message,
+        });
+      });
+  }
+);
 
 module.exports = router;
